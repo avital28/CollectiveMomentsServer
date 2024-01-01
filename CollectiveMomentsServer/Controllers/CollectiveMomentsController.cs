91ddc6c077e84af57ee58b0116546547390ff2c2
User <User@LAB3-3>
2024-01-01 12:27:06 +0200

    Updated

--- CollectiveMomentsServer/Controllers/CollectiveMomentsController.cs
+++ CollectiveMomentsServer/Controllers/CollectiveMomentsController.cs
@@ -1,4 +1,5 @@
-﻿using CollectiveMomentsServerBL.Models;
+﻿using CollectiveMomentsServer.DTO_Models;
+using CollectiveMomentsServerBL.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
@@ -63,5 +64,53 @@ namespace CollectiveMomentsServer.Controllers
 
             
         }
+        [Route("UpdateUser")]
+        [HttpPost]
+        public async Task<ActionResult<User>> UpdateUserAsync([FromBody] UserDto usr )
+        {
+            try
+            {
+                User u = context.Users.Find(usr.Id);
+                if (u != null)
+                {
+                    if(await UpdateUser(u, usr))
+                    
+                    return Ok(usr);
+                    return BadRequest();
+                }
+                else
+                    return Forbid();
+            }
+            catch (Exception ex)
+            {
+
+
+            }
+            return BadRequest();
+        }
+
+        private async Task<bool>  UpdateUser (User u, UserDto user)
+        {
+            
+                if (user.Firstname!=null)
+                    u.FirstName = user.Firstname;
+                if (user.Lastname != null)
+                    u.LastName = user.Lastname;
+                if (user.Passwrd != null)
+                    u.Passwrd = user.Passwrd;
+                if (user.Email != null)
+                    u.Email = user.Email;
+                if (user.Username != null)
+                    u.UserName = user.Username;
+                try { await context.SaveChangesAsync(); return true; }
+            catch (Exception ex) { return false; }    
+
+            
+
+
+
+        }
     }
+
+    
 }
---
+++ CollectiveMomentsServer/DTO Models/UserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CollectiveMomentsServer.DTO_Models
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Passwrd { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
