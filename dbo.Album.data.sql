9135de2d0a8a6df90c622391ca92a9885848bfc3
User <User@LAB3-3>
2024-04-08 12:37:42 +0300

    /

---
+++ dbo.Album.data.sql
@@ -0,0 +1,8 @@
+﻿SET IDENTITY_INSERT [dbo].[Album] ON
+INSERT INTO [dbo].[Album] ([ID], [AlbumCover], [Longitude], [Latitude], [AdminID], [AlbumTitle]) VALUES (1, N'1.jpg', N'-122.084', N'37.421998333333335', 0, NULL)
+INSERT INTO [dbo].[Album] ([ID], [AlbumCover], [Longitude], [Latitude], [AdminID], [AlbumTitle]) VALUES (2, N'2.jpg', N'-122.084', N'37.421998333333335', 0, NULL)
+INSERT INTO [dbo].[Album] ([ID], [AlbumCover], [Longitude], [Latitude], [AdminID], [AlbumTitle]) VALUES (3, N'3.jpg', N'-122.084', N'37.421998333333335', 0, NULL)
+INSERT INTO [dbo].[Album] ([ID], [AlbumCover], [Longitude], [Latitude], [AdminID], [AlbumTitle]) VALUES (4, N'4.jpg', N'-122.084', N'37.421998333333335', 0, NULL)
+INSERT INTO [dbo].[Album] ([ID], [AlbumCover], [Longitude], [Latitude], [AdminID], [AlbumTitle]) VALUES (5, N'5.jpg', N'-122.084', N'37.421998333333335', 0, NULL)
+INSERT INTO [dbo].[Album] ([ID], [AlbumCover], [Longitude], [Latitude], [AdminID], [AlbumTitle]) VALUES (6, N'6.jpg', N'-122.084', N'37.421998333333335', 0, NULL)
+SET IDENTITY_INSERT [dbo].[Album] OFF
